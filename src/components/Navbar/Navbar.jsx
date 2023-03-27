dd17dea7907987164e70ba4357e04b641a405e56
McMillan <117737074+BootCodex@users.noreply.github.com>
2023-03-27 15:40:37 +0200

    imports

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,9 +1,12 @@
 import React from 'react';
+import { GiHamburgerMenu } from 'react-icons/gi';
+import { MdOutlineRestaurantMenu } from 'react-icons/md';
+
 import './Navbar.css';
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <div className=''></div>
   )
 }
 
